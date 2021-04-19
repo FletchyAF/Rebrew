d443b5c2eccd426298ba3bee50894fbfb7b47bda
FletchyAF <joshua.f1998@gmail.com>
2021-04-19 16:47:43 -0400

    Correcting Inproper Routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resources( :brewery_reviews)
   end
   
-  resources :brewery_reviews do
+  resources :brewery_reviews do 
     resources (:breweries)
     resources (:users)
   end
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   post '/logout', to: "sessions#destroy"
-  patch '/breweries/:brewery_id/brewery_reviews/', to: "brewery_reviews#update"
+  # patch '/breweries/:brewery_id/brewery_reviews/', to: "brewery_reviews#update"
   get "/auth/google_oauth2/callback", to: "sessions#create_with_google"
   get "/beareries", to: "breweries#beareries"
 end
